52fd17800cc85ff1b3026cc5d28a4fed2cebff3b
THE D MAN FOR TG <90522855+THE-DMAN@users.noreply.github.com>
2021-11-03 13:22:57 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -22,7 +22,7 @@ from database.filters_mdb import(
 BUTTONS = {}
 
 
-@Client.on_message(filters.group & filters.text)
+@Client.on_message(filters.group & filters.text & filters.incoming)
 async def give_filter(client,message):
     group_id = message.chat.id
     name = message.text
